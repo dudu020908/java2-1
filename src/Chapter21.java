4cfaecb8f0690c435e6c2beda16a06b96839eb23
dudu020908 <lygdleh@naver.com>
2024-03-22 12:48:22 +0900

    1장, 2장

---
+++ src/Chapter21.java
@@ -0,0 +1,19 @@
+
+
+public class Chapter21 {
+    public static int sum(int n,int m){
+        return n+m;
+    }
+    public static void main(String[] args) {
+        int i = 20;
+        int s;
+        char a;
+        s = sum(i,10);
+        a = '?';
+        var pi = 3.14;
+        System.out.println(pi);
+        System.out.println("Hello"+13);
+        System.out.println(s);
+    }
+}
+
