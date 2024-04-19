c258f6c4416c5007e589db83fe4019b9d46fd59f
dudu020908 <lygdleh@naver.com>
2024-04-19 12:22:44 +0900

    패키지

---
+++ src/test/Calc.java
@@ -0,0 +1,7 @@
+package test;
+
+public abstract class Calc {
+    public abstract int add(int a,int b);
+    public abstract int substract(int a, int b);
+    public abstract double average(int[]a);
+}
